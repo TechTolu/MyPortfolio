e147d40c81aa5dd1060fe4458a806ae4904c7e30
Tolulope Ogunnusi <tolulopeogunnusi@gmail.com>
2022-08-04 12:48:15 +0100

    update

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import Portfolio from './components/portfolio/Portfolio.js'
 import Contact from './components/contact/Contact.js'
 import Footer from './components/footer/Footer.js'
 
+
 const App = () => {
   return (
-    <>
+    <div>
         <Header/>
         <Nav/>
         <About/>
@@ -18,7 +19,7 @@ const App = () => {
         <Portfolio/>
         <Contact/>
         <Footer/>
-    </>
+    </div>
   )
 }
 
